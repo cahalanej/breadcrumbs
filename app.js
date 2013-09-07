d1ff9929a73846b2738a76462b2eef57d794b978
Andrew Ma <4ndrew@gmail.com>
2013-09-07 15:17:41 -0400

    twilio fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express')
   , app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
-  , twilio = require('twilio' ); //Load the twilio module
+  , twilio = require('twilio'); 
 
 
 server.listen(8080);
@@ -23,10 +23,10 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-Create a new REST API client to make authenticated requests against the
+// Create a new REST API client to make authenticated requests against the
 // twilio back end
 var accountSid = 'AC71b7fa31feefaf648cda86b85d68fe2d'
-var authToken = "{{ c6195524b11918be9586a42d407b605f }}"
+var authToken = 'c6195524b11918be9586a42d407b605f'
 var client = new twilio.RestClient(accountSid, authToken);
 
 // Pass in parameters to the REST API using an object literal notation. The
@@ -40,16 +40,16 @@ client.sms.messages.create({
 // function will be called when a response is received from Twilio
 // The "error" variable will contain error information, if any.
 // If the request was successful, this value will be "falsy"
-if (!error) {
+	if (!error) {
 // The second argument to the callback will contain the information
 // sent back by Twilio for the request. In this case, it is the
 // information about the text messsage you just sent:
-console.log('Success! The SID for this SMS message is:');
-console.log(message.sid);
+		console.log('Success! The SID for this SMS message is:');
+		console.log(message.sid);
 
-console.log('Message sent on:');
-console.log(message.dateCreated);
-}
+		console.log('Message sent on:');
+		console.log(message.dateCreated);
+	}
 else {
     console.log('Oops! There was an error.');
 }
